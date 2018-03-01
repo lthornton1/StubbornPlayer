f07386d9d0a5f088b205bb888ad9d94bcac4ae5f
lthornton1 <lthornton1@mail.csuchico.edu>
2018-02-28 17:17:00 -0800

    winner winner

--- main.cpp
+++ main.cpp
@@ -5,7 +5,25 @@ using namespace std;
 
 int main()
 {
+
   SecretDoor game;
-    
+  int play;
+  int win=0;
+
+  cout << "How many times would you like to play?"<<endl;
+  cin >> play;
+  for (int i=0; i<=play; i++){
+
+    game.newGame();
+    game.guessDoorC();
+    game.guessDoorC();
+    if (game.isWinner() == true){
+      win++;
+    }
+
+  } 
+
+  cout<<"You won "<<win<<" times!"<<endl;
+
   return 0;
 }
